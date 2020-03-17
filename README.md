6f36a37dd2c2990dd5c788436033fe8108a8684b
WouterJanssens98 <wouter.janssens@student.arteveldehs.be>
2020-03-17 19:12:31 +0100

    added Matrix, readme...

---
+++ README.md
@@ -0,0 +1,7 @@
+Maak een webapplicatie m.b.v. Client (HTML, CSS en JavaScript), Server (Python Interpreter / Server on Raspberry PI: matrix.py en environment.py) en Google Firebase.
+
+Server
+environment.py Om x-aantal minuten worden de waarden van de omgevingssensoren doorgestuurd naar Firebase. matrix.py De server luistert naar wijzigingen in matrix-document binnen Firebase
+
+Client
+Luistert naar wijzigingen in environment-document binnen Firebase Stuurt wijzigingen van de matrix (kleur) door naar Firebase
--- sensehat_dashboard/pi/matrix.py
+++ sensehat_dashboard/pi/matrix.py
@@ -4,20 +4,32 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 
-cred = credentials.Certificate("./config/labo-i-firebase-adminsdk-nr652-7d2e873b71.json")
+COLLECTION = 'raspberry'
+
+DOCUMENT = 'lector-pi'
+
+
+
+cred = credentials.Certificate("../config/labo-i-firebase-adminsdk-nr652-7d2e873b71.json")
 
 firebase_admin.initialize_app(cred)
 
-#connect firestore
+# sensehat 
+sense = SenseHat()
+sense.set_imu_config(False, False, False)
+sense.clear()
 
+def update_sensehat(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        doc_readable = doc.to_dict()
+        print(doc_readable)
+
+# connect firestore
 db = firestore.client()
+pi_ref = db.collection(COLLECTION).document(DOCUMENT)
+pi_watch = pi_ref.on_snapshot(update_sensehat)
 
-def on_snapshot(doc_snapshot, changes, read_time):
-  for doc in doc_snapshot:
-    doc_dict = doc.to_dict()
-    print(doc_dict)
-
-# Get the pi ref
-pi_ref = db.collection(COLLECTION_NAME).document(PI_ID)
-pi_watch = pi_ref.on_snapshot(on_snapshot)
+# app
+while True:
+    pass
 
